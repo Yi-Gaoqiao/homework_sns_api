79c5ed27454ca0c8118804fdd66ebfcc18faa4e8
Tsubasa Takahashi <2bsttas@gmail.com>
2020-08-15 00:19:29 +0900

    modified urls.py in sns_project.

--- sns-api/sns_project/urls.py
+++ sns-api/sns_project/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/user/', include('api_user.urls')),
-    # path('api/dm/', include('api_dm.urls')),
-    # path('api/post/', include('api_post.urls')),
+    path('api/user/', include('api_user.urls')),
+    path('api/dm/', include('api_dm.urls')),
+    path('api/post/', include('api_post.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
